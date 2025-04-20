2c16ba305a5432bbeb7495b9c9411a151733326b
LWaring30 <waringlj@g.cofc.edu>
2025-04-20 16:20:42 -0400

    Added WIP Svelte script, WIP shopping cart and additional css

---
+++ tickets.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { preprocess } from "svelte/compiler";
+/*import ironCart from*/
+import x from 
+
+let { data } = $state();
+
+let cartOpen = $state(false)
+</script>
